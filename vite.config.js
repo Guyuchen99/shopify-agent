20606f70771442ee5385377314952cf434e62845
Yuchen Gu <guyuchen999@gmail.com>
2025-09-03 15:05:06 -0600

    Redo Config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.jsx",
+      name: "MyWidget",
+      fileName: (format) => `my-widget.${format}.js`,
+      formats: ["umd"],
+    },
+  },
 });
