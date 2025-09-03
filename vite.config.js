d447c43f37b1bb15c9d0a9ee978e6c5e6ae0ca74
Yuchen Gu <guyuchen999@gmail.com>
2025-09-03 15:23:30 -0600

    Updated Config

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,14 @@ export default defineConfig({
       fileName: (format) => `my-widget.${format}.js`,
       formats: ["umd"],
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
 });
